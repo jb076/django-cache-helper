5ad9206712421cde3203ac812b561f7ce800cc37
Ryan Kuczka <ryan.kuczka@gmail.com>
2015-03-04 16:11:28 -0500

    Add test_project and travis
    test_project is needed to run tests without running in an existing
    django project

---
+++ test_project/test_project/settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'django_cache_helper_test.db',
+    }
+}
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '-&_@yi2)i^algv-6vjt(+mi*(s9rk7r4v!s2c2s+-$131xq#_t'
+
+MIDDLEWARE_CLASSES = ()
+
+INSTALLED_APPS = ()
