730c3c1b616216374695b6d70d33331bc3182bb5
Ryan Kuczka <ryan.kuczka@gmail.com>
2015-03-04 16:27:18 -0500

    travis: Use django-nose to actually find tests on django 1.5

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -11,3 +11,7 @@ SECRET_KEY = '-&_@yi2)i^algv-6vjt(+mi*(s9rk7r4v!s2c2s+-$131xq#_t'
 MIDDLEWARE_CLASSES = ()
 
 INSTALLED_APPS = ()
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = ['--verbosity=2', '--nocapture']
